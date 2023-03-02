4ee732c07ec46f117c3e389d066f0428b8ad7a3c
rafaellasm <rafaelladossantosmarquess@gmail.com>
2023-03-02 11:46:11 -0300

    Primeiro Projeto

---
+++ TJ-Sushi/frmComponentes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TJ_Sushi
+{
+    public partial class frmComponentes : Form
+    {
+        public frmComponentes()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
